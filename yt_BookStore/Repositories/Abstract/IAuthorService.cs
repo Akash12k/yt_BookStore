42153bb7709409938e264a747a9953f764ebca46
Akash <Akash@AKASH_PC>
2024-06-08 17:12:22 +0530

    Add project files.

---
+++ yt_BookStore/Repositories/Abstract/IAuthorService.cs
@@ -0,0 +1,13 @@
+﻿using yt_BookStore.Models.Domain;
+
+namespace yt_BookStore.Repositories.Abstract
+{
+    public interface IAuthorService
+    {
+        bool Add(Author model);
+        bool Update(Author model);
+        bool Delete(int id);
+        Author FindById(int id);
+        IEnumerable<Author> GetAll();
+    }
+}
---
+++ yt_BookStore/Repositories/Abstract/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using yt_BookStore.Models.Domain;
+
+namespace yt_BookStore.Repositories.Abstract
+{
+    public interface IBookService
+    {
+        bool Add(Book model);
+        bool Update(Book model);
+        bool Delete(int id);
+        Book FindById(int id);
+        IEnumerable<Book> GetAll();
+    }
+}
